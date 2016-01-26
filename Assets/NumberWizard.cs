8cba458faf1fb5583b275f57b97f2c91a96a9efa
Brandon Charest <brandon_charest@yahoo.com>
2016-01-25 21:50:02 -0500

    first commit

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;	
+	//public int maxGuessAllowed=10;
+	
+	public Text text;
+	
+	void Start () {
+		startGame();
+	}
+	
+	void startGame(){
+		max=1000;
+		min=1;		
+		nextGuess();
+
+	}
+		
+	public void guessHigher(){
+		min=guess;		
+		nextGuess();	
+	}	
+	
+	public void guessLower(){
+		max=guess;		
+		nextGuess();
+	}	
+	
+	void nextGuess(){	
+		guess=Random.Range(min, max+1);
+		text.text=guess.ToString();		
+		//maxGuessAllowed=maxGuessAllowed-1;
+				
+		//if(maxGuessAllowed<=0){
+		//	Application.LoadLevel("Win");
+		//}	
+	}
+}
